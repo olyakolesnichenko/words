134918bdc11fc1bbad39c23495763a9a7a26f2b0
olyakolesnichenko <kolesnichenko.zp@gmail.com>
2019-10-09 21:24:10 +0300

    add localization

--- client/src/scenes/MainPage/index.jsx
+++ client/src/scenes/MainPage/index.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import { withTranslation } from 'react-i18next';
 import { connect } from 'react-redux';
 import { fetchWords } from '../../routines/routines';
 import { Button, Grid } from 'semantic-ui-react';
@@ -13,9 +14,7 @@ class MainPage extends Component {
   }
 
   render() {
-    const { loading, word } = this.props;
-
-    console.log(word);
+    const { loading, word, t } = this.props;
 
     return loading ? (
       <Spinner />
@@ -24,12 +23,12 @@ class MainPage extends Component {
         <section className={styles.main}>
           <Grid centered container columns={1}>
             <Grid.Column computer={13} mobile={16}>
-              <h1 className={styles.headerCentered}>Do you know this word?</h1>
+              <h1 className={styles.headerCentered}>{t('Do you know this word?')} </h1>
               <p className={styles.mainText}>
                 {word}
               </p>
-              <Button primary>YES</Button>
-              <Button secondary>NO</Button>
+              <Button primary>{t('YES')}</Button>
+              <Button secondary>{t('NO')}</Button>
             </Grid.Column>
           </Grid>
         </section>
@@ -49,7 +48,8 @@ class MainPage extends Component {
 MainPage.propTypes = {
   loading: PropTypes.bool.isRequired,
   word: PropTypes.string.isRequired,
-  fetchWords: PropTypes.func.isRequired
+  fetchWords: PropTypes.func.isRequired,
+  t: PropTypes.any.isRequired
 };
 
 const mapStateToProps = ({
@@ -69,4 +69,4 @@ const mapDispatchToProps = {
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(MainPage);
+)(withTranslation()(MainPage));
