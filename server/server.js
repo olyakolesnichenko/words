63abd24dc8a2025628a34cbfc95e14c1f9b45c04
olyakolesnichenko <kolesnichenko.zp@gmail.com>
2020-03-09 22:44:37 +0200

    Pull request fixes

--- server/server.js
+++ server/server.js
@@ -8,24 +8,20 @@ app.get('/', function (req, res) {
 });
 app.get('/word', function (req, res) {
   const words = [
-    {"word":'eternity', "translation": 'вечность'},
-    {"word":'passion', "translation": 'страсть'},
-    {"word":'smile', "translation": 'улыбка'},
-    {"word":'fantastic', "translation": 'фантастика'},
-    {"word":'destiny', "translation": 'судьба'},
-    {"word":'freedom', "translation": 'свобода'},
-    {"word":'liberty', "translation": 'свобода'},
-    {"word":'tranquility', "translation": 'спокойствие'}];
-  res.status(200).json({word: words[Math.floor(Math.random()*words.length)]});
+    {'word': 'eternity', 'translation': 'вечность'},
+    {'word': 'passion', 'translation': 'страсть'},
+    {'word': 'smile', 'translation': 'улыбка'},
+    {'word': 'fantastic', 'translation': 'фантастика'},
+    {'word': 'destiny', 'translation': 'судьба'},
+    {'word': 'freedom', 'translation': 'свобода'},
+    {'word': 'liberty', 'translation': 'свобода'},
+    {'word': 'tranquility', 'translation': 'спокойствие'}];
+  res.status(200).json({word: words[Math.floor(Math.random() * words.length)]});
 });
 app.post('/answer', function (req, res) {
   const answerData = req.body;
-console.log(req.body);
-  // if (answerData.word) {
-    res.status(200).json({result: true});
-  // } else {
-  //   res.status(400).json({result: false});
-  // }
+  console.log(req.body);
+  res.status(200).json({result: true});
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
