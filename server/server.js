2e1dffba1824f1a3ce74cb9f93805d0878621d45
olya.kolesnichenko <kolesnichenko.zp@gmail.com>
2020-03-09 22:46:22 +0200

    Add word translation & hint (#2)
    * Add word translation & hint
    
    * Pull request fixes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var app = express();
 
+app.use(express.json());
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 app.get('/word', function (req, res) {
-  res.status(200).json({word: 'Shekel'});
+  const words = [
+    {'word': 'eternity', 'translation': 'вечность'},
+    {'word': 'passion', 'translation': 'страсть'},
+    {'word': 'smile', 'translation': 'улыбка'},
+    {'word': 'fantastic', 'translation': 'фантастика'},
+    {'word': 'destiny', 'translation': 'судьба'},
+    {'word': 'freedom', 'translation': 'свобода'},
+    {'word': 'liberty', 'translation': 'свобода'},
+    {'word': 'tranquility', 'translation': 'спокойствие'}];
+  res.status(200).json({word: words[Math.floor(Math.random() * words.length)]});
+});
+app.post('/answer', function (req, res) {
+  const answerData = req.body;
+  console.log(req.body);
+  res.status(200).json({result: true});
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
